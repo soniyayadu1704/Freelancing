3c4310608f332c799a3c20a85130eb35298819a8
soniyayadu1704 <soniyayadu1704@gmail.com>
2026-05-21 19:15:37 +0530

    Portfolio website completed

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import {
+BrowserRouter,
+Routes,
+Route
+} from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+
+import Home from "./pages/Home"
+
+import Contact from "./components/Contact"
+
+import HireUs from "./pages/HireUs"
+
+export default function App(){
+
+return(
+
+<BrowserRouter>
+
+<div
+className="
+min-h-screen
+bg-gradient-to-b
+from-[#faf8ff]
+to-[#f1edff]
+"
+>
+
+<Navbar/>
+
+<Routes>
+
+<Route
+path="/"
+element={<Home/>}
+/>
+
+<Route
+path="/contact"
+element={<Contact/>}
+/>
+
+<Route
+path="/hire"
+element={<HireUs/>}
+/>
+
+</Routes>
+
+</div>
+
+</BrowserRouter>
+
+)
+
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import Hero from "../components/Hero"
+import About from "../components/About"
+import WhyChoose from "../components/WhyChoose"
+import Projects from "../components/Projects"
+
+export default function Home(){
+
+return(
+
+<>
+
+<Hero/>
+
+<About/>
+
+<WhyChoose/>
+
+<Projects/>
+
+</>
+
+)
+
+}
